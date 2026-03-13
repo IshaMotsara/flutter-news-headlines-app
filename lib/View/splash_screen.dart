deea46ca071680a4b38827a080faafce959c37b9
Isha motsara <141553936+IshaMotsara@users.noreply.github.com>
2026-03-13 10:08:14 +0530

    create splash screen

---
+++ lib/View/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:news_headlines_flutter/View/home_screen.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<SplashScreen> {
+  @override
+  void initState(){
+    super.initState();
+    Timer(Duration(seconds:2),(){
+      Navigator.push(context,MaterialPageRoute(builder:(context)=>HomeScreen()));
+    });
+  }
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+final width = MediaQuery.of(context).size.width;
+    return  Scaffold(
+    body:Container(
+      child:Column(
+        children: [
+        Image.asset('images/splash_pic.jpg',
+        fit:BoxFit.cover,
+        width:width*.9,
+        height:height*.5,
+        ),
+        ],
+      )
+      ,)
+    );
+  }
+}
